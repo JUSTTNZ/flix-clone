91a160ea6819dd32fbd19c37ece968bfcbaa3169
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-02-11 08:16:36 +0100

    added a gitignore file and updated some files

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function mobileLink(){
+    let mobile = document.getElementById("show");
+    mobile.style.transform="translate(0%)  ";
+}
+
+function mobileClose(){
+    let mobile = document.getElementById("show");
+    mobile.style.transform="translate(-100%)  ";
+}
+
